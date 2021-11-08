67567dc563150b9075dab4ed798600277efd54e2
jakleh <jslehner16@gmail.com>
2021-11-07 18:37:39 -0600

    Completed private Auth and polished front end

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,6 +2,9 @@ class EntriesController < ApplicationController
   before_action :set_entry, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: [:index, :show]
 
+  #Adding "before" action to only allow users to edit/update/destroy their own entries...
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   # GET /entries or /entries.json
   def index
     @entries = Entry.all
@@ -13,7 +16,8 @@ class EntriesController < ApplicationController
 
   # GET /entries/new
   def new
-    @entry = Entry.new
+    #@entry = Entry.new
+    @entry = current_user.entries.build
   end
 
   # GET /entries/1/edit
@@ -22,7 +26,10 @@ class EntriesController < ApplicationController
 
   # POST /entries or /entries.json
   def create
-    @entry = Entry.new(entry_params)
+    #@entry = Entry.new(entry_params)
+
+    #Updating create method to reflect the user-entry auth change...
+    @entry = current_user.entries.build(entry_params)
 
     respond_to do |format|
       if @entry.save
@@ -55,6 +62,12 @@ class EntriesController < ApplicationController
       format.html { redirect_to entries_url, notice: "Entry was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  #Adding method to determine whether some entry can be changed by the current user...
+  def correct_user
+    @entry = current_user.entries.find_by(id: params[:id])
+    redirect_to entries_path, notice: "Not Authorized to edit this entry" if @entry.nil?
   end
 
   private
