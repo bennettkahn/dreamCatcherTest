daae6685e08b9db0a8ae5174a06fb28ba12b7083
Garrett Gilliom <garrettgilliom@gmail.com>
2021-11-09 11:30:55 -0600

    Deleted keywords model to prep for tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_151040) do
+ActiveRecord::Schema.define(version: 2021_11_04_143940) do
 
   create_table "entries", force: :cascade do |t|
     t.string "title"
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(version: 2021_11_04_151040) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.string "keyword"
-    t.string "user"
-  end
-
-  create_table "keywords", force: :cascade do |t|
-    t.integer "text_id"
-    t.string "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
