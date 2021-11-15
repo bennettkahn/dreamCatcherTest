8227d9473c901be85db047410674b63c4019285d
Garrett Gilliom <garrettgilliom@gmail.com>
2021-11-15 00:00:21 -0600

    Fixed author logic in explore

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          has_many :entries 
   def self.id_to_name id
     user = User.where(id: id)
-    if user.nil?
+    if user.length() == 0
       return "Anonymous"
     end
     return user[0].email # replace when first/last names are added
